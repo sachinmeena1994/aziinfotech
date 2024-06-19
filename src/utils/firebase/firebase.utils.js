6741e84535eb0b80130afef6d8b264f9632a6115
sachinmeena0794 <sachin.meena@indianic.com>
2024-06-20 01:46:07 +0530

    modified:   src/utils/firebase/firebase.utils.js

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from 'firebase/auth';
+import { doc, getFirestore, getDoc, setDoc} from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyD9YA4XPhGD55Rpr09fW9RCCeOl9wbn4AI",
@@ -24,11 +25,27 @@ provider.setCustomParameters({
   prompt: 'select_account',
 });
 
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
-
-  console.log(userAuth);
-};
-
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth)=>{
+  const userDocRef = doc(db, "user", userAuth.uid)
+  const userSnapshot = await getDoc(userDocRef);
+  if(!userSnapshot.exists()){
+    const {displayName, email} = userAuth;
+    try{
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        role: "user",
+        createAt : new Date()
+      });
+    }
+    catch(error){
+      console.log(error, "error")
+    }
+  }
+  
+return userDocRef
+}
