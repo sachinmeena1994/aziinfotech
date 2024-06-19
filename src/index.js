ef8fc60a97a97e878843ec1e9c8646a72fa9e7af
sachinmeena0794 <sachin.meena@indianic.com>
2024-06-20 02:27:41 +0530

    UserContenxt Created

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.scss';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+
+import './index.scss';
+import {UserProvider} from './components/contexts/user.context';
+import App from './App';
 
 const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <UserProvider>
+    <App />
+    </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
   rootElement
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,12 +1,20 @@
+import { useState, useContext } from 'react';
+import { UserContext } from '../../components/contexts/user.context';
+
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+
+  const {currentUser,setCurrentUser} = useContext(UserContext)
   const logGoogleUser = async () => {
     const {user} = await signInWithGooglePopup();
     createUserDocumentFromAuth(user)
+    setCurrentUser(user)
+    console.log(currentUser)
+    
   };
 
   return (
