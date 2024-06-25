6d968f0b43338530c58af0612aec14555313af3e
Vishwesh Dhoble <vishweshdhoble0610@gmail.com>
2024-06-25 15:38:35 +0530

    adding burger menu

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
+        {/* <Route index element={<Home />} /> */}
         <Route path='shop' element={<Shop />} />
         <Route path='sign-in' element={<SignIn />} />
       </Route>
