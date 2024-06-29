c7253f26e3041052697ec2d9030620c0dbfcf400
Rishabh Malewar <rishumalewar@gmail.com>
2024-06-29 12:26:03 +0530

    partial integration of product display page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import SignIn from './routes/sign-in/sign-in.component';
+import ProductDisplay from './routes/product-display/product-display.component';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -15,6 +16,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
         <Route path='sign-in' element={<SignIn />} />
+        <Route path='product-display' element={<ProductDisplay />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/product-display/product-display.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductPage from "../../components/product-page/product-page.component";
+
+const ProductDisplay = () => {
+  return (
+    <>
+      <ProductPage />
+    </>
+  );
+};
+
+export default ProductDisplay;
